a04cc068cc7e800b05bc399e19fcad452fbddb94
Sivaiah sankula <sivaiah.sankula@xcubelabs.com>
2023-05-16 22:38:51 +0530

    time and disabled btns and added chats2 below

--- src/app/graphs/graphs.component.ts
+++ src/app/graphs/graphs.component.ts
@@ -13,24 +13,81 @@ export class GraphsComponent implements OnInit, AfterViewInit {
   btnDisabled = false
   chart:any;
   chartScan:any
+  chartLiveAmp:any
   chartData:any = []
   isScanStarted:boolean = false
+  isScanEnable:boolean = false
   isShowScanImage:boolean = false
+  clickedRecord:any
+  isClickedAny:boolean = false
+  scanTime:any
   constructor() { }
 
 
   getApiResponse = () =>{
 
-    this.isScanStarted = true
+    if(this.chartScan){
+    this.chartScan.destroy();
+    this.chartScan = null;
+    }
+    if(this.chartLiveAmp){
+      this.chartLiveAmp.destroy()
+    this.chartLiveAmp = null;
+    }
 
-    this.chart = new ApexCharts(document.querySelector("#chart"), {
-      series: [{
-        data: this.chartData
-      }],
+
+    this.chartData = []
+    this.isScanStarted = true;
+    this.isScanEnable = true;
+    const currentDate = new Date();
+
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const year = currentDate.getFullYear();
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+    const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+    this.scanTime = formattedDate;
+    this.chart = new ApexCharts(document.querySelector('#chart'), {
+      series: [
+        {
+          data: this.chartData,
+        },
+      ],
       chart: {
         type: 'bar',
         height: 200,
-        width:400
+        width: 400,
+        events: {
+          dataPointSelection: (event: any, chartContext: any, config: any) => {
+              if(this.chartLiveAmp){
+                this.chartLiveAmp.destroy()
+              this.chartLiveAmp = null;
+              }
+              console.log(config.dataPointIndex, config);
+              this.isClickedAny = true;
+              this.clickedRecord = config.dataPointIndex;
+
+              this.chartLiveAmp = new ApexCharts(
+                document.querySelector('#chart_liveAmp'),
+                {
+                  series: [
+                    {
+                      data: [this.chartData[config.dataPointIndex]],
+                    },
+                  ],
+                  chart: {
+                    type: 'bar',
+                    height: 200,
+                    width: 300,
+                  },
+                }
+              );
+
+              this.chartLiveAmp.render();
+          },
+        },
       },
       xaxis: null,
     });
@@ -40,9 +97,7 @@ export class GraphsComponent implements OnInit, AfterViewInit {
     this.btnDisabled = true
     this.socket.onmessage = (e:any)=>{
       // console.log(e.data)
-      
-      this.tableRes.push(parseInt(e.data))
-      console.log(this.tableRes)
+     
       this.updateChartData(parseInt(e.data))
     }
     this.socket.onclose = (e:any)=>{
@@ -73,8 +128,9 @@ export class GraphsComponent implements OnInit, AfterViewInit {
     this.tableRes = []
     this.btnDisabled = false
     this.isShowScanImage = true
+    this.isScanEnable = false
 
-
+    console.log(this.chartData)
     this.chartScan = new ApexCharts(document.querySelector("#chart_scan"), {
       series: [{
         data: this.chartData
@@ -84,7 +140,7 @@ export class GraphsComponent implements OnInit, AfterViewInit {
         height: 170,
         width:800
       },
-      xaxis: null,
+    
     });
   
     this.chartScan.render();
